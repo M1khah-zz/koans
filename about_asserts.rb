33f5dc95fbee10c6d53db3ac10884dc37e005400
m1khah <alexeyenko92@gmail.com>
2016-10-09 23:38:47 +0300

    completed

--- about_asserts.rb
+++ about_asserts.rb
@@ -27,14 +27,15 @@ class AboutAsserts < Neo::Koan
 
   # Some ways of asserting equality are better than others.
   def test_a_better_way_of_asserting_equality
-    expected_value = actual_value
+
     actual_value = 1 + 1
+    expected_value = actual_value
 
     assert_equal expected_value, actual_value
   end
 
   # Sometimes we will ask you to fill in the values
   def test_fill_in_values
-    assert_equal __, 1 + 1
+    assert_equal 2, 1 + 1
   end
 end
